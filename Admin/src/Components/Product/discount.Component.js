fc0888aae7867e27148445bd5560e7c411d63553
nphoa7777@gmail.com <nphoa7777@gmail.com>
2018-07-31 16:29:58 -0700

    rfdetrer

--- Admin/src/Components/Product/discount.Component.js
+++ Admin/src/Components/Product/discount.Component.js
@@ -9,7 +9,14 @@ class DiscountProductComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
- 
+      filter:{
+        productName:"",
+        productCode:"",
+        categoryId:"",
+        authorId:"",
+        publisher:"",
+        companyReleaseId:""
+      }
     };
   }
   
@@ -17,8 +24,25 @@ class DiscountProductComponent extends Component {
      
   }
  
- 
+  handleChange = (event) => {
+    // this.setState({
+    //   filter:{
+    //     [event.target.name]:event.target.value
+    //   }
+    // })
 
+    this.setState(prevState => ({
+       filter: {
+            ...prevState.filter,
+            [event.target.name]:event.target.value
+        }
+    }))
+  }
+
+  handleSubmitFilter = (event) => {
+    event.preventDefault();
+    console.log(this.state.filter);
+  }
   
   render() {
     if(this.state.redirectProduct){
@@ -33,33 +57,73 @@ class DiscountProductComponent extends Component {
         </div>
         <div className="forms">
           <div className="form-three widget-shadow">
-            <form className="form-horizontal">
+            <form className="form-horizontal" onSubmit={this.handleSubmitFilter}>
            
                 
                 <div class="panel panel-info" style={{padding:'0px'}}>
                 <div class="panel-heading">Filter Information</div>
                 <div class="panel-body">
                     <br/>
-                    <div className="form-group">
-                        <label className="col-sm-2 control-label">Email</label>
-                        <div className="col-sm-8">
-                          
-                        </div>
-                    </div>
+                    <div className='row'>
+                        <div className="col-sm-6">
+                          <div className="form-group">
+                          <label className="col-sm-4 control-label">Product Code</label>
+                          <div className="col-sm-6">
+                              <input type="text" className="form-control" name="productCode" onChange={this.handleChange}/>
+                          </div>
+                          </div>
 
-                    <div className="form-group">
-                        <label className="col-sm-2 control-label">Password</label>
-                        <div className="col-sm-8">
-                           
-                        </div>
-                    </div>
+                          <div className="form-group">
+                              <label className="col-sm-4 control-label">Product Name</label>
+                              <div className="col-sm-6">
+                                <input type="text" className="form-control" name="productName"/>
+                              </div>
+                          </div>
 
-                    <div className="form-group">
-                        <label className="col-sm-2 control-label">Password confirm</label>
-                        <div className="col-sm-8">
-                     
+                          <div className="form-group">
+                              <label className="col-sm-4 control-label">Category Id</label>
+                              <div className="col-sm-3">
+                                <select type="text" className="form-control" name="categoryParentId">
+                                    <option>A</option>      
+                                </select>
+                              </div>
+                              <div className="col-sm-3">
+                                <select type="text" className="form-control" name="categoryId">
+                                    <option>A</option>      
+                                </select>
+                              </div>
+                          </div>
+
+                        </div>
+                        <div className="col-sm-6">
+                          <div className="form-group">
+                                <label className="col-sm-4 control-label">Author</label>
+                                <div className="col-sm-6">
+                                  <select type="text" className="form-control" name="authorId">
+                                        <option>A</option>
+                                  </select>
+                                </div>
+                          </div>
+                          <div className="form-group">
+                                <label className="col-sm-4 control-label">Publisher</label>
+                                <div className="col-sm-6">
+                                  <select type="text" className="form-control" name="publisherId">
+                                        <option>A</option>
+                                  </select>
+                                </div>
+                          </div>
+                          <div className="form-group">
+                                <label className="col-sm-4 control-label">Company release</label>
+                                <div className="col-sm-6">
+                                  <select type="text" className="form-control" name="companyReleaseId">
+                                        <option>A</option>
+                                  </select>
+                                </div>
+                          </div>
                         </div>
                     </div>
+                 
+                   
                 </div>
             </div>
         
